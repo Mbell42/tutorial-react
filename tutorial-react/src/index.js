f42d4f7a59932e78b62f473a2053dab0e0899865
Matthew Bell <matthewbell1030@gmail.com>
2021-01-14 17:13:28 -0800

    began process of lifting state from squares up to board

--- tutorial-react/src/index.js
+++ tutorial-react/src/index.js
@@ -16,17 +16,29 @@ class Square extends React.Component {
       return (
         <button
          className="square"
-         onClick={() => this.setState({value: 'x'})}
+         onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
