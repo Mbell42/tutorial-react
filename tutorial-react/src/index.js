3bc51217f01bceaccfbaa2b4ed166195dbff2db7
Matthew Bell <matthewbell1030@gmail.com>
2021-01-15 16:52:14 -0800

    updated board component to display winner and to short-circuit handleClick function if there is a winner.

--- tutorial-react/src/index.js
+++ tutorial-react/src/index.js
@@ -24,6 +24,9 @@ function Square(props) {
 
     handleClick(i) {
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       // console.log(squares);
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({squares: squares,
@@ -41,7 +44,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.square);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
