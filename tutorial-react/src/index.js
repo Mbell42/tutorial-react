47355dc7cd2fa188fe0f1512129670451fb50fa5
Matthew Bell <matthewbell1030@gmail.com>
2021-01-15 18:53:40 -0800

    added history state to Game component, in process of lifting state up from Board component

--- tutorial-react/src/index.js
+++ tutorial-react/src/index.js
@@ -76,6 +76,16 @@ function Square(props) {
   }
   // Game
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true,
+      };
+    }
+    
     render() {
       return (
         <div className="game">
