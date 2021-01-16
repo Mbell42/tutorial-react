eee355245db09ee0449ada197775637e659b3639
Matthew Bell <matthewbell1030@gmail.com>
2021-01-15 22:26:37 -0800

    began updating Game component to be able to keep track of move history.

--- tutorial-react/src/index.js
+++ tutorial-react/src/index.js
@@ -1,8 +1,9 @@
 // DEPENDENCIES
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { unstable_concurrentAct } from 'react-dom/test-utils';
 import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 // COMPONENTS
 // Square
@@ -54,13 +55,45 @@ function Square(props) {
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
+    }
+    handleClick(i) {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares
+        }]),
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+    jumpTo(step) {
+      this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+      });
     }
     render() {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move #' + move : 'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
       let status;
       if (winner) {
         status= 'Winner: ' + winner;
@@ -77,7 +110,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
